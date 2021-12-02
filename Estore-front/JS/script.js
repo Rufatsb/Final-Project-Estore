62d884b35c6e1ad312a99b749570c11f2f26ef54
Rufatsb <rufatsb@code.edu.az>
2021-12-02 11:59:27 +0400

    Main navbar completed,mobile navbar created and added some styles

--- Estore-front/JS/script.js
+++ Estore-front/JS/script.js
@@ -1,15 +1,24 @@
-$(document).ready(function() {
-    var scroll_start = 0;
-    var startchange = $('#startchange');
-    var offset = startchange.offset();
-    if (startchange.length) {
-        $(document).scroll(function() {
-            scroll_start = $(this).scrollTop();
-            if (scroll_start > offset.top) {
+// $(document).ready(function() {
+//     var scroll_start = 0;
+//     var startchange = $('#startchange');
+//     var offset = startchange.offset();
+//     if (startchange.length) {
+//         $(document).scroll(function() {
+//             scroll_start = $(this).scrollTop();
+//             if (scroll_start > offset.top) {
 
-            } else {
+//             } else {
 
-            }
-        });
+//             }
+//         });
+//     }
+// });
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
     }
-});
+}
