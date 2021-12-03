66dcdbc60e32bec47b2e4a0bad87414a64e84e10
Rufatsb <rufatsb@code.edu.az>
2021-12-03 11:58:27 +0400

    Mobile navbar,sticky navbar exactly is ready.

--- Estore-front/JS/script.js
+++ Estore-front/JS/script.js
@@ -1,24 +1,62 @@
-// $(document).ready(function() {
-//     var scroll_start = 0;
-//     var startchange = $('#startchange');
-//     var offset = startchange.offset();
-//     if (startchange.length) {
-//         $(document).scroll(function() {
-//             scroll_start = $(this).scrollTop();
-//             if (scroll_start > offset.top) {
+$(document).ready(function() {
 
-//             } else {
+    let firstul = $(".firstul")
+    let firstanchor = $(".firstanchor")
+    let secondul = $(".secondul")
+    let secondanchor = $(".secondanchor")
+    let thirdul = $(".thirdul")
+    let thirdanchor = $(".thirdanchor")
 
-//             }
-//         });
-//     }
-// });
+
+    firstanchor.click(function togglefunction() {
+        firstul.toggle();
+    });
+    secondanchor.click(function togglefunction() {
+        secondul.toggle();
+    });
+    thirdanchor.click(function togglefunction() {
+        thirdul.toggle();
+    });
+
+
+
+
+});
+
+
 
 function myFunction() {
     var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+    var y = document.querySelector(".mobile-navbar-menu")
+    if (y.style.height == "100%" && x.style.height == "100%") {
+        y.style.height = "47px";
+        x.style.height = "0%";
     } else {
-        x.style.display = "block";
+
+        y.style.height = "100%";
+        x.style.height = "100%";
+    }
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() { FixedFunction() };
+
+// Get the navbar
+var navbar = document.querySelector(".bottom-header");
+var mobilenavbar = document.querySelector(".mobile-navbar")
+    // Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+var msticky = mobilenavbar.offsetTop;
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function FixedFunction() {
+    if (window.scrollY >= sticky) {
+        navbar.classList.add("sticky")
+
+    } else if (window.scrollY >= msticky) {
+        mobilenavbar.classList.add("msticky")
+    } else {
+        navbar.classList.remove("sticky");
+        mobilenavbar.classList.remove("msticky");
+
     }
 }
