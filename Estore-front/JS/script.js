cf307afc4c89aeaeb597b5338ca4e9ca47d843d0
Rufatsb <rufatsb@code.edu.az>
2021-12-07 06:49:27 +0400

    Home category section completed

--- Estore-front/JS/script.js
+++ Estore-front/JS/script.js
@@ -29,6 +29,32 @@ $(document).ready(function() {
 
     });
 
+    // external js: isotope.pkgd.js
+
+    // init Isotope
+    var $grid = $('.filterdiv').isotope({
+        itemSelector: '.single-product',
+        layoutMode: 'fitRows'
+    });
+    // filter functions
+
+    // bind filter button click
+    $('.filters-button-group').on('click', 'button', function() {
+        var filterValue = $(this).attr('data-filter');
+        // use filterFn if matches value
+        filterValue = filterFns[filterValue] || filterValue;
+        $grid.isotope({ filter: filterValue });
+    });
+    // change is-checked class on buttons
+    $('.button-group').each(function(i, buttonGroup) {
+        var $buttonGroup = $(buttonGroup);
+        $buttonGroup.on('click', 'button', function() {
+            $buttonGroup.find('.is-checked').removeClass('is-checked');
+            $(this).addClass('is-checked');
+        });
+    });
+
+
 
 
 });
