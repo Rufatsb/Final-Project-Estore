316ff10c495aa1d6180d46f752fbc0cc1667bd22
Rufatsb <rufatsb@code.edu.az>
2021-12-19 03:05:23 +0400

    Top scroll button added,mobile navbar open bug fixed,sticky navbar bug fixed

--- Estore-front/JS/script.js
+++ Estore-front/JS/script.js
@@ -139,7 +139,10 @@ function myFunction() {
 }
 
 
-window.onscroll = function() { FixedFunction() };
+window.onscroll = function() {
+    FixedFunction();
+    scrollFunction()
+};
 var navbar = document.querySelector(".bottom-header");
 var mobilenavbar = document.querySelector(".mobile-navbar")
 var sticky = navbar.offsetTop;
@@ -147,11 +150,33 @@ var sticky = navbar.offsetTop;
 function FixedFunction() {
     if (window.scrollY >= sticky) {
         navbar.classList.add("sticky")
+        navbar.classList.remove("mt-1")
     } else {
         navbar.classList.remove("sticky");
-
+        navbar.classList.add("mt-1")
 
     }
+}
+
+
+//Get the button:
+topscrollbtn = document.getElementById("topscrollbtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+        topscrollbtn.style.display = "block";
+    } else {
+        topscrollbtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
 filterSelection("all")
