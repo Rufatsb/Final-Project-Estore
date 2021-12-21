2a087f27af3609de25de885bad59a00f9f3dc08c
Rufatsb <rufatsb@code.edu.az>
2021-12-21 09:44:22 +0400

    Backend project created,sql server connection established,controllers created,index actions and index views created,banner viewcomponent created,some style bugs fixed

---
+++ Estore-back/Estore/Estore/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Estore.DAL;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Estore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ Estore-back/Estore/Estore/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using Estore.DAL;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Estore.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ContactController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
